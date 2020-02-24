13ccdd66078b9b98f341a2da2fb8323455dcb710
johanhiguita <higuitajohan@gmail.com>
2020-02-24 00:41:16 -0500

    implements instance methods in account model

--- api/controllers/account.js
+++ api/controllers/account.js
@@ -6,6 +6,10 @@ async function index(req, res, next) {
     try {
         let accounts = await Account.find().select("-__v");
         accounts = await helper.add_extra_props(accounts); //add incomes, expenses, balance
+
+        //add extra props using model's methods
+
+
         console.log(accounts);
         res.status(200).json(accounts);
     } catch (error) {
@@ -21,6 +25,17 @@ async function show(req, res, next) {
     try {
         let account = await Account.findById(id).select("-__v");
         if (account) {
+
+            const expenses = await account.get_total_expenses();
+            const incomes = await account.get_total_incomes();
+            const balance = incomes - expenses;
+
+            account = account.toObject(); //mongoose objects does not allow change props
+
+            account["expenses"] = expenses;
+            account["incomes"] = incomes;
+            account["balance"] = balance;
+
             res.status(200).json(account);
         } else {
             res.status(404).json({
--- api/models/account.js
+++ api/models/account.js
@@ -1,41 +1,71 @@
 const mongoose = require("mongoose");
 
 const accountSchema = mongoose.Schema(
-  {
-    _id: mongoose.Types.ObjectId,
-    name: {
-      type: String,
-      required: [true, "No name provided"],
-      trim: true
+    {
+        _id: mongoose.Types.ObjectId,
+        name: {
+            type: String,
+            required: [true, "No name provided"],
+            trim: true
+        },
+        type: {
+            type: String,
+            enum: ["cash", "savings", "credit card"],
+            required: [true, "No type provided"]
+        }
     },
-    type: {
-        type: String,
-        enum: ['cash', 'savings', "credit card"],
-        required: [true, "No type provided"],
-    },
-  },
 
-  { timestamps: {} }
+    { timestamps: {} }
 );
 
 /* account methods */
 
-accountSchema.methods.get_balance = function(id, cb) {
-   /*  const Payment = require("./payment");
+accountSchema.methods.get_transactions = function(id, cb) {
+    const Transaction = require("./transaction");
 
-    return Payment.find({ project: id })
-      .select("-__v")
-      .exec()
-      .then(cb)
-      .catch(error => console.log(error)); */
-  };
+    return Transaction.find({ account: id })
+        .exec()
+        .then(cb)
+        .catch(error => console.log(error));
+};
 
-  accountSchema.methods.get_total_incomes = function (id, cb){
 
-  }
+accountSchema.methods.get_total_expenses = async function(since = "this-month") {
+    const Transaction = require("./transaction");
+    let expenses = 0;
 
-  accountSchema.methods.get_total_expenses = function (id, cb){
+    try {
+        /* spend this month, get all transactions for this account  */
+        var transactions = await Transaction.find({account: this._id, type: "expense"}).select("type amount");
+    } catch (error) {
+        console.log(error);
+    }
 
-  }
+    transactions.forEach(trans => {expenses += trans.amount});
+    return expenses;
+
+
+};
+
+accountSchema.methods.get_total_incomes = async function(since = "this-month") {
+
+    const Transaction = require("./transaction");
+    let incomes = 0;
+
+    try {
+        /* spend this month, get all transactions for this account  */
+        var transactions = await Transaction.find({account: this._id, type: "income"}).select("type amount");
+    } catch (error) {
+        console.log(error);
+    }
+
+    transactions.forEach(trans => {incomes += trans.amount});
+
+    return incomes;
+
+};
+
+accountSchema.methods.get_balance = function(id, cb) {};
+
 
 module.exports = mongoose.model("account", accountSchema);
